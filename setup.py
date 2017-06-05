0f13d11db92bde3b1bd7fd1a6a5ffc4076053b2b
lcleme9871 <l.clem99@gmail.com>
2017-06-05 17:35:58 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nsga2',
+      version='0.1',
+      description='NSGA-II non dominated sorting algorithm implementation',
+      author='Lee',
+      author_email='l.cleme99@gmail.com,
+      url = "https://github.com/lcleme9871,
+      packages=['nsga2', 'nsga2.problems', 'nsga2.problems.zdt', 'metrics', 'metrics.problems'],
+     )
